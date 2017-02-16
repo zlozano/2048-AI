4a94c0d5ec621c4b121ef07f77a6b1bb2f346893
ZACHARY T. LOZANO <ztlozano@us.ibm.com>
2017-02-16 09:38:25 -0600

    best so far. about 50% success rate

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -3,10 +3,10 @@ from BaseAI_3 import BaseAI
 from Grid_3 import Grid
 import time
 
-time_limit = 0.02
+time_limit = 0.05
+
 
 class PlayerAI(BaseAI):
-
     def getMove(self, grid: Grid):
         return self.alpha_beta(grid)
 
@@ -14,6 +14,7 @@ class PlayerAI(BaseAI):
     An iterative deepening, alpha-beta pruned, minimax search.
     Return the next move which minimizes the maximum loss incurred.
     """
+
     def alpha_beta(self, node: Grid):
         maximizing_value = -math.inf
         maximizing_move = None
@@ -29,6 +30,7 @@ class PlayerAI(BaseAI):
     """
     Maximizing function - returns the best possible move, given the result of the minimizing player's move
     """
+
     def maximizing(self, alpha, beta, node: Grid, depth):
         maximizing_move = None
         if depth == 0:
@@ -53,6 +55,7 @@ class PlayerAI(BaseAI):
     """
     Minimizing function - returns the lowest scored move based on opponent's previous move
     """
+
     def minimizing(self, alpha, beta, node: Grid, depth):
         if depth == 0:
             return self.heuristic(node)
@@ -75,55 +78,78 @@ class PlayerAI(BaseAI):
     """
     Maximizing heuristic function for non-terminal nodes
     """
+
     @staticmethod
     def heuristic(node: Grid):
         score = 0
+        num_empty_cells = len(node.getAvailableCells())
 
-        # award monotonic sequences among rows
-        for row in node.map:
-            for i in range(1, len(row)):
-                if i < len(row) - 1:
-                    if row[i - 1] <= row[i] <= row[i + 1]:
-                        score += 900
-                    elif row[i - 1] >= row[i] >= row[i + 1]:
-                        score += 900
-                    else:
-                        score -= 100000
-                if row[i] != 0 and row[i - 1] != 0:
-                    score -= abs(row[i] - row[i - 1]) * 1000
+        for i in range(node.size):
+            for j in range(node.size):
+                val = node.getCellValue((i, j))
+                score += val * 10 if val else (1000 / num_empty_cells)  # reward empty spaces more if they are scarce
 
-        # award monotonic sequences among columns
-        for j in range(node.size):
-            for i in range(1, len(node.map)):
-                if i < len(node.map) - 1:
-                    if node.map[i - 1][j] <= node.map[i][j] <= node.map[i + 1][j]:
-                        score += 900
-                    elif node.map[i - 1][j] >= node.map[i][j] >= node.map[i + 1][j]:
-                        score += 900
-                    else:
-                        score -= 100000
-                if node.map[i][j] != 0 and node.map[i - 1][j] != 0:
-                    score -= abs(node.map[i][j] - node.map[i - 1][j]) * 1000
+                val_up = node.getCellValue((i - 1, j))
+                val_dn = node.getCellValue((i + 1, j))
+                val_lt = node.getCellValue((i, j - 1))
+                val_rt = node.getCellValue((i, j + 1))
+                if val_up is not None and \
+                   val_dn is not None and \
+                   val != 0:
+                    if not (val_up <= val <= val_dn or val_up >= val >= val_dn):
+                        score -= 75 * (abs(val_up - val) + abs(val_dn - val))
+                if val_lt is not None and \
+                   val_rt is not None and \
+                   val != 0:
+                    if not(val_lt <= val <= val_rt or val_lt >= val >= val_rt):
+                        score -= 75 * (abs(val_lt - val) + abs(val_rt - val))
 
-        # maximize board real estate (as many high value cells with maximum empty cells
-        for row in node.map:
-            for val in row:
-                if val == 0:
-                    score += 30000
-                elif val == 2 or val == 4:
-                    score -= 8000
-                elif val >= 128:
-                    score += val * 10000
-                elif val >= 32:
-                    score += val * 100
+                if j == 0:
+                    row = node.map[i]
+                    monotonic_row = all(x <= y for x, y in zip(row, row[1:]))
+                    monotonic_row = monotonic_row or all(x >= y for x, y in zip(row, row[1:]))
+                    if monotonic_row:
+                        score += 200 * sum(row)  # big bonus
+
+                if i == 0:
+                    col = [node.map[pos][j] for pos in range(node.size)]
+                    monotonic_col = all(x <= y for x, y in zip(col, col[1:]))
+                    monotonic_col = monotonic_col or all(x >= y for x, y in zip(col, col[1:]))
+                    if monotonic_col:
+                        score += 200 * sum(col)  # big bonus
+
+                val_order = PlayerAI.get_value_order(val)
+                if val_order > 0:
+                    val_up_order = PlayerAI.get_value_order(val_up)
+                    val_dn_order = PlayerAI.get_value_order(val_dn)
+                    val_lt_order = PlayerAI.get_value_order(val_lt)
+                    val_rt_order = PlayerAI.get_value_order(val_rt)
+                    if val_up_order > 0:
+                        score -= abs(val_up_order - val_order) * 25
+                    if val_dn_order > 0:
+                        score -= abs(val_dn_order - val_order) * 25
+                    if val_lt_order > 0:
+                        score -= abs(val_lt_order - val_order) * 25
+                    if val_rt_order > 0:
+                        score -= abs(val_rt_order - val_order) * 25
 
         return score
+
+    @staticmethod
+    def get_value_order(value):
+        order = 0
+        if value is not None and value > 0:
+            while value > 1:
+                value >>= 1
+                order += 1
+        return order
 
     """
     Determine if a losing state has been found or a winning state.
     For winning state return highest possible value.
     For losing state return lowest possible value.
     """
+
     @staticmethod
     def get_terminal_node_value(node: Grid):
         for row in node.map:
@@ -131,5 +157,3 @@ class PlayerAI(BaseAI):
                 if val == 2048:
                     return math.inf
         return -math.inf
-
-
